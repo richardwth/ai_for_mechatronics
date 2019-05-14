48abdf77b17b8e06fe4e62558be543b4695d1659
richard_wang <richard_wang_wth@outlook.com>
2019-05-14 11:05:16 +1000

    update inaturalist code

--- GeneralTools/inaturalist_func.py
+++ GeneralTools/inaturalist_func.py
@@ -1,168 +1,112 @@
 """
-This file contains functions and classes to read data
+This file contains functions and classes for saving, reading and processing the iNaturalist 2019 dataset
+
 """
+from GeneralTools.my_input import _bytes_feature, _int64_feature
+from GeneralTools.misc_fun import FLAGS
+import sys
+import time
+import os.path
 import tensorflow as tf
 import numpy as np
-import os.path
-from GeneralTools.misc_fun import FLAGS
 
 
-########################################################################
-# define macro
-# FloatList, Int64List and BytesList are three base feature types
-def _float_feature(value):
-    return tf.train.Feature(
-        float_list=tf.train.FloatList(value=[value])
-        if isinstance(value, float) else tf.train.FloatList(value=value))
+def images_to_tfrecords(image_names, output_filename, num_images_per_tfrecord, image_class=None, target_size=299):
+    """ This function converts images listed in the image_names to tfrecords files
 
-
-def _int64_feature(value):  # numpy int is not int!
-    return tf.train.Feature(
-        int64_list=tf.train.Int64List(value=[value])
-        if isinstance(value, int) else tf.train.Int64List(value=value))
-
-
-def _bytes_feature(value):
-    return tf.train.Feature(
-        bytes_list=tf.train.BytesList(value=[value])
-        if isinstance(value, (str, bytes)) else tf.train.BytesList(value=value))
-    # return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-
-########################################################################
-def create_placeholder(tensor, dtype=None):
-    """ This function creates a placeholder for tensor.
-    When the input tensor is a list/tuple of tensors, it creates a list of placeholders
-
-    :param tensor: input tensor must be one of the following types: np.ndarray, tuple or list of np.ndarray
-    :param dtype: a basic TensorFlow DType defined in tf.dtypes.DType.
-    If provided, the placeholder will have the given dtype;
-    otherwise, the dtype is inferred.
-    When tensor is a list or tuple, dtype must be a basic DType, list or tuple.
+    :param image_names: a list of strings like ['xxx.jpg', 'xxx.jpg', 'xxx.jpg', ...]
+    :param output_filename: 'train'
+    :param num_images_per_tfrecord: integer
+    :param image_class: class label for each image, a list like [1, 34, 228, ...]
+    :param target_size: the size of images after padding and resizing
     :return:
     """
-    if isinstance(tensor, np.ndarray):
-        if dtype is None:
-            if tensor.dtype in {np.float32, np.float64, np.float16, np.float}:
-                placeholder = tf.placeholder(tf.float32, tensor.shape)
-            elif tensor.dtype in {np.int, np.int32, np.int64}:
-                placeholder = tf.placeholder(tf.int32, tensor.shape)
-            else:
-                raise NotImplementedError('The dtype {} is not implemented.'.format(tensor.dtype))
+    from PIL import Image, ImageOps
+    from io import BytesIO
+
+    num_images = len(image_names)
+    # iteratively handle each image
+    writer = None
+    start_time = time.time()
+    for image_index in range(num_images):
+        # retrieve a single image
+        im_loc = os.path.join(FLAGS.DEFAULT_DOWNLOAD, image_names[image_index])
+        im_cla = image_class[image_index] if isinstance(image_class, list) else None
+        im = Image.open(im_loc)
+
+        # resize the image
+        old_size = im.size
+        ratio = float(target_size) / max(old_size)
+        if not ratio == 1.0:
+            new_size = tuple([int(x * ratio) for x in old_size])
+            im = im.resize(new_size, Image.LANCZOS)
+
+        # zero-pad the images
+        new_size = im.size
+        delta_w = target_size - new_size[0]
+        delta_h = target_size - new_size[1]
+        if delta_w < 0 or delta_h < 0:
+            raise AttributeError('The target size is smaller than the image size {}.'.format(new_size))
+        elif delta_w > 0 or delta_h > 0:
+            padding = (delta_w // 2, delta_h // 2, delta_w - (delta_w // 2), delta_h - (delta_h // 2))
+            im = ImageOps.expand(im, padding)
+
+        # if image not RGB format, convert to RGB
+        # This is done in case the image is greyscale
+        if im.mode != 'RGB':
+            im = im.convert('RGB')
+
+        # convert the full image data to the jpeg compressed string to reduce tfrecord size
+        with BytesIO() as fp:
+            im.save(fp, format="JPEG")
+            im_string = fp.getvalue()
+
+        # im.show('a new image')
+        # save to tfrecords
+        if image_index % num_images_per_tfrecord == 0:
+            file_out = "{}_{:03d}.tfrecords".format(output_filename, image_index // num_images_per_tfrecord)
+            # print("Writing on:", file_out)
+            writer = tf.python_io.TFRecordWriter(file_out)
+        if image_class is None:
+            # for test set, the labels are unknown and not provided
+            instance = tf.train.Example(
+                features=tf.train.Features(feature={
+                    'x': _bytes_feature(im_string)
+                }))
         else:
-            placeholder = tf.placeholder(dtype, tensor.shape)
-    elif isinstance(tensor, tf.Tensor):
-        raise TypeError('The input to placeholder cannot be tf.Tensor.')
-    elif isinstance(tensor, (list, tuple)):
-        if isinstance(dtype, (list, tuple)):
-            placeholder = tuple([create_placeholder(
-                single_tensor, single_dtype) for single_tensor, single_dtype in zip(tensor, dtype)])
-        else:
-            placeholder = tuple([create_placeholder(
-                single_tensor, dtype) for single_tensor in tensor])
-    else:
-        raise NotImplementedError(
-            'Placeholder can only be created for numpy array, tf.Tensor, list or tuple')
+            instance = tf.train.Example(
+                features=tf.train.Features(feature={
+                    'x': _bytes_feature(im_string),
+                    'y': _int64_feature(im_cla)
+                }))
+        writer.write(instance.SerializeToString())
+        if image_index % 2000 == 0:
+            sys.stdout.write('\r {}/{} instances finished.'.format(image_index + 1, num_images))
+        if image_index % num_images_per_tfrecord == (num_images_per_tfrecord - 1):
+            writer.close()
 
-    return placeholder
-
-
-########################################################################
-class DatasetFromTensor(object):
-    """
-    This class provides an example of defining a customized input pipeline.
-    """
-    def __init__(self, data, dtype=None, map_func=None, name='Input'):
-        """ Initialize a dataset and applies map_func
-
-        :param data: Input data must be from the following types: numpy array,
-        tuple and list. Placeholder will be automatically created for data or each element in data.
-        :param dtype: the dtype of tensor, can be a basic basic DType, list or tuple.
-        :param map_func: common map_func includes scale and reshape
-        :param name:
-        """
-        self.name = name
-        # check input types
-        assert isinstance(data, (np.ndarray, tf.Tensor, tuple, list)), \
-            '{}: Data must be from the following types: numpy array, tuple and list.'.format(self.name)
-
-        with tf.name_scope(name=self.name) as self.scope:
-            # create placeholder and initialize the dataset
-            self.placeholder = create_placeholder(data, dtype)
-            self.feed_dict = {}
-            for key, value in zip(self.placeholder, data):
-                self.feed_dict[key] = value
-            self.num_samples = data.shape[0] if isinstance(data, np.ndarray) else data[0].shape[0]
-            self.dataset = tf.data.Dataset.from_tensor_slices(self.placeholder)
-            # apply map_func
-            if map_func is not None:
-                self.dataset = self.dataset.map(map_func)
-        self.iterator = None
-
-        if FLAGS.VERBOSE:
-            print('{} dataset output type is {}'.format(
-                self.name, self.dataset.output_types))
-            print('{} dataset output shape is {}'.format(
-                self.name, self.dataset.output_shapes))
-
-    def schedule(self, batch_size, num_epoch=-1, skip_count=None, shuffle=True, buffer_size=10000):
-        with tf.name_scope(self.scope):
-            if skip_count is None:
-                skip_count = self.num_samples % batch_size
-            if skip_count > 0:
-                self.dataset = self.dataset.skip(skip_count)
-                if FLAGS.VERBOSE:
-                    print('{}: Number of {} instances skipped.'.format(
-                        self.name, skip_count))
-            # shuffle
-            if shuffle:
-                self.dataset = self.dataset.shuffle(buffer_size)
-            # make batch
-            self.dataset = self.dataset.batch(batch_size)
-            # repeat datasets for num_epoch
-            self.dataset = self.dataset.repeat(num_epoch)
-            # initialize an iterator
-            self.iterator = self.dataset.make_initializable_iterator()
-
-        return self  # facilitate method cascading
-
-    def next(self, batch_size=None):
-        if self.iterator is None:
-            assert batch_size is not None, \
-                '{}: Batch size must be provided.'.format(self.name)
-            self.schedule(batch_size)
-        # somehow set_shape does not work as expected.
-        # data_batch = self.iterator.get_next()
-        # output_shapes = [[batch_size] + output_shape.as_list()[1:] for output_shape in self.dataset.output_shapes]
-        # for index, each_shape in enumerate(output_shapes):
-        #     data_batch[index].set_shape(each_shape)
-        # print(data_batch)
-        # return data_batch
-        with tf.name_scope(self.scope):
-            return self.iterator.get_next()
-
-    def initializer(self):
-        assert self.iterator is not None, \
-            '{}: Batch must be provided.'.format(self.name)
-        return self.iterator.initializer, self.feed_dict
+    writer.close()
+    duration = time.time() - start_time
+    sys.stdout.write('\n All {} instances finished in {:.1f} seconds'.format(num_images, duration))
 
 
 ########################################################################
 class ReadTFRecords(object):
     def __init__(
-            self, filenames, num_features=None, num_labels=0, x_dtype=tf.string, y_dtype=tf.int64, batch_size=64,
+            self, filenames, num_features=None, num_labels=0, x_dtype=tf.string, y_dtype=tf.int64, batch_size=16,
             skip_count=0, file_repeat=1, num_epoch=None, file_folder=None,
-            num_threads=8, buffer_size=10000, shuffle_file=False):
+            num_threads=8, buffer_size=2000, shuffle_file=False,
+            decode_jpeg=False):
         """ This function creates a dataset object that reads data from files.
 
-        :param filenames: e.g., cifar
-        :param num_features: e.g., 3*64*64
-        :param num_labels:
+        :param filenames: string or list of strings, e.g., 'train_000', ['train_000', 'train_001', ...]
+        :param num_features: e.g., 3*299*299
+        :param num_labels: 0 or positive integer
         :param x_dtype: default tf.string, the dtype of features stored in tfrecord file
-        :param y_dtype:
-        :param num_epoch:
+        :param y_dtype: default tf.int64, the dtype of labels stored in tfrecord file
+        :param num_epoch: integer or None
         :param buffer_size:
-        :param batch_size:
+        :param batch_size: integer
         :param skip_count: if num_instance % batch_size != 0, we could skip some instances
         :param file_repeat: if num_instance % batch_size != 0, we could repeat the files for k times
         :param num_epoch:
@@ -170,6 +114,7 @@ class ReadTFRecords(object):
         :param num_threads:
         :param buffer_size:
         :param shuffle_file: bool, whether to shuffle the filename list
+        :param decode_jpeg: if input is saved as JPEG string, set this to true
 
         """
         if file_folder is None:
@@ -197,6 +142,7 @@ class ReadTFRecords(object):
         self.batch_shape = [self.batch_size, self.num_features]
         self.num_epoch = num_epoch
         self.skip_count = skip_count
+        self.decode_jpeg = decode_jpeg
         # read data,
         dataset = tf.data.TFRecordDataset(filenames)  # setting num_parallel_reads=num_threads decreased the performance
         self.dataset = dataset.map(self.__parser__, num_parallel_calls=num_threads)
@@ -226,8 +172,13 @@ class ReadTFRecords(object):
         # decode examples
         datum = tf.parse_single_example(example_proto, features=proto_config)
         if self.x_dtype == tf.string:  # if input is string / bytes, decode it to float32
-            # first decode data to uint8, as data is stored in this way
-            datum['x'] = tf.decode_raw(datum['x'], tf.uint8)
+            if self.decode_jpeg:
+                # first decode compressed image string to uint8, as data is stored in this way
+                # datum['x'] = tf.image.decode_image(datum['x'], channels=3)
+                datum['x'] = tf.image.decode_jpeg(datum['x'], channels=3)
+            else:
+                # first decode data to uint8, as data is stored in this way
+                datum['x'] = tf.decode_raw(datum['x'], tf.uint8)
             # then cast data to tf.float32
             datum['x'] = tf.cast(datum['x'], tf.float32)
             # cannot use string_to_number as there is only one string for a whole sample
@@ -255,7 +206,7 @@ class ReadTFRecords(object):
 
     ###################################################################
     def shape2image(self, channels, height, width, resize=None):
-        """ This function shapes the input instance to image tensor
+        """ This function shapes the input instance to image tensor.
 
         :param channels:
         :param height:
@@ -266,11 +217,22 @@ class ReadTFRecords(object):
         """
 
         def image_preprocessor(image):
-            # scale image to [-1,1]
-            image = tf.subtract(tf.divide(image, 127.5), 1)
+            # scale image to [0, 1] or [-1,1]
+            image = tf.divide(image, 255.5, name='scale_range')
+            # image = tf.subtract(tf.divide(image, 127.5), 1)
+
             # reshape - note this is determined by how the data is stored in tfrecords, modify with caution
-            image = tf.reshape(image, (channels, height, width)) \
-                if FLAGS.IMAGE_FORMAT == 'channels_first' else tf.reshape(image, (height, width, channels))
+            if self.decode_jpeg:
+                # if decode_jpeg is true, the image is already in [height, width, channels] format,
+                # thus we only need to consider IMAGE_FORMAT
+                if FLAGS.IMAGE_FORMAT == 'channels_first':
+                    image = tf.transpose(image, perm=(2, 0, 1))
+                pass
+            else:
+                # if decode_jpeg is false, the image is probably in vector format
+                # thus we reshape the image according to the stored format provided by IMAGE_FORMAT
+                image = tf.reshape(image, (channels, height, width)) \
+                    if FLAGS.IMAGE_FORMAT == 'channels_first' else tf.reshape(image, (height, width, channels))
             # resize
             if isinstance(resize, (list, tuple)):
                 if FLAGS.IMAGE_FORMAT == 'channels_first':
@@ -351,7 +313,7 @@ class ReadTFRecords(object):
             if self.num_epoch is None:
                 self.dataset = self.dataset.repeat()
             else:
-                FLAGS.print('Num_epoch set: {} epochs.'.format(num_epoch))
+                print('Num_epoch set: {} epochs.'.format(num_epoch))
                 self.dataset = self.dataset.repeat(self.num_epoch)
 
             self.iterator = self.dataset.make_one_shot_iterator()
@@ -364,6 +326,8 @@ class ReadTFRecords(object):
         :param sample_same_class: if the data must be sampled from the same class at one iteration
         :param sample_class: if provided, the data will be sampled from class of this label, otherwise,
             data of a random class are sampled.
+            The sample_class is compared against the first label in num_labels.
+            Note that sample_class should be smaller than the num of classes in total.
             Note that class_label should not be provided during training.
         :param shuffle_data:
         :return:
@@ -380,9 +344,6 @@ class ReadTFRecords(object):
             if sample_class is not None:
                 assert isinstance(sample_class, (np.integer, int)), \
                     'class_label must be integer.'
-                assert sample_class < self.num_labels, \
-                    'class_label {} is larger than maximum value {} allowed.'.format(
-                        sample_class, self.num_labels - 1)
                 sample_same_class = True
             if not self.scheduled:
                 self.scheduler(
